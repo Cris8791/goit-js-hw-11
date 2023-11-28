bbb3d90490c2dd9e66e17268635f8842faf39f73
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2023-11-28 18:30:23 +0200

    commit_2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 import * as Notiflix from 'notiflix';
 
 document.addEventListener('DOMContentLoaded', function () {
   const searchForm = document.getElementById('search-form');
   const gallery = document.querySelector('.gallery');
-  const loadMoreButton = document.querySelector('.load-more');
   let currentPage = 1;
   let totalHits = 0;
+  let lightbox;
 
   searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
     const searchQuery = event.target.elements.searchQuery.value.trim();
-
-    if (searchQuery === '') {
-      return; // Evităm căutarea goală
-    }
-
-    // Resetăm pagina la 1 la fiecare căutare nouă
     currentPage = 1;
-    fetchData(searchQuery);
-  });
-
-  loadMoreButton.addEventListener('click', function () {
-    currentPage++;
-    const searchQuery = searchForm.elements.searchQuery.value.trim();
     fetchData(searchQuery);
   });
 
   async function fetchData(searchQuery) {
     try {
       const apiKey = '40947481-13c540b45e3fd4f57e1ee7dde';
-      const perPage = 40; // Modificat pentru a afișa 40 de imagini pe pagină
+      const perPage = 40;
 
       const response = await axios.get('https://pixabay.com/api/', {
         params: {
@@ -47,9 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
       totalHits = response.data.totalHits;
-
       const imageData = response.data.hits;
-
       const imagesArray = imageData.map(image => ({
         webformatURL: image.webformatURL,
         largeImageURL: image.largeImageURL,
@@ -60,53 +46,57 @@ document.addEventListener('DOMContentLoaded', function () {
         downloads: image.downloads,
       }));
 
-      console.log('Images Array:', imagesArray);
-
       if (currentPage === 1) {
-        // Dacă suntem pe prima pagină, golim galeria
         gallery.innerHTML = '';
-        // Ascundem butonul "Load more"
-        loadMoreButton.style.display = 'none';
       }
 
       if (imagesArray.length > 0) {
-        // Adăugăm cardurile de imagini în galerie
         imagesArray.forEach(image => {
           const card = createImageCard(image);
           gallery.appendChild(card);
         });
 
-        // Inițializează SimpleLightbox
-        // const lightbox = new SimpleLightbox('.gallery a');
+        if (!lightbox) {
+          lightbox = new SimpleLightbox('.gallery a');
+        } else {
+          lightbox.refresh();
+        }
 
-        // Afișăm butonul "Load more" dacă mai există imagini
         if (
           imagesArray.length === perPage &&
           totalHits > perPage * currentPage
         ) {
-          loadMoreButton.style.display = 'block';
+          currentPage++;
         } else {
-          loadMoreButton.style.display = 'none';
-          // Afișăm notificarea cu numărul total de imagini
           Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
         }
       } else {
         console.log('Matricea de imagini este goală.');
-        // Afișăm o notificare dacă nu sunt găsite imagini
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
     } catch (error) {
       console.error('Error fetching data:', error);
-      // Afișăm o notificare pentru erori
       Notiflix.Notify.failure('Error fetching data. Please try again.');
     }
   }
 
+  window.addEventListener('scroll', function () {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+      const searchQuery = searchForm.elements.searchQuery.value.trim();
+      fetchData(searchQuery);
+    }
+  });
+
   function createImageCard(image) {
     const card = document.createElement('div');
     card.className = 'photo-card';
+
+    const link = document.createElement('a');
+    link.href = image.largeImageURL;
+    link.className = 'lightbox';
 
     const img = document.createElement('img');
     img.src = image.webformatURL;
@@ -126,7 +116,8 @@ document.addEventListener('DOMContentLoaded', function () {
     info.appendChild(comments);
     info.appendChild(downloads);
 
-    card.appendChild(img);
+    link.appendChild(img);
+    card.appendChild(link);
     card.appendChild(info);
 
     return card;
